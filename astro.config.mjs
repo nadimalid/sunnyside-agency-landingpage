e87c5de8542f80c0b1628a22c67f2a67e192f822
Nadim <nadimalid@gmail.com>
2024-02-15 16:15:39 +0300

    add actions to deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    site: 'https://nadimalid.github.io',
+    base: '/sunnyside-agency-landingpage',
+    integrations: [tailwind()]
 });
